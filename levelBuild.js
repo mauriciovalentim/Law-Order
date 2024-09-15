0eb737b3a6c06f5f0e00bee1b4ca96c7d2f4c870
Mauricio Valentim <mauriciovalentim.dev@gmail.com>
2024-09-14 21:39:43 -0300

    update

---
+++ levelBuild.js
@@ -0,0 +1,252 @@
+import { sceneTransition } from "../animations.js";
+import { getLevels } from "../levels.js";
+
+let id = 0;
+let points = 0;
+let levels = getLevels();
+
+const questionText = document.querySelector("#question");
+const btnContainer = document.querySelector("#button-container");
+const answers = document.querySelectorAll(".answer");
+setLevel();
+
+function setLevel() {
+    questionText.textContent = levels[id].question;
+    answers.forEach(showText);
+}
+
+function showText(item, index) {
+    item.innerText = levels[id].options[index].text;
+}
+
+const buttons = document.querySelectorAll(".answer-button");
+buttons.forEach(btnAction);
+
+function btnAction(button, index) {
+    button.addEventListener("click", () => {
+        //travar botões
+        btnContainer.style.pointerEvents = "none";
+        btnContainer.style.opacity = "0.5";
+        if (index + 1 === levels[id].rightOption) {
+            //ganhar
+            points += 1;
+            id += 1;
+            if (levels[id]) {
+                btnContainer.style.pointerEvents = "auto";
+                btnContainer.style.opacity = "1";
+                setLevel();
+            } else {
+                endGame();
+            }
+            //verificar se ainda há perguntas
+            //caso sim
+            //ir para a próxima pergunta
+            //destravar botões
+            //caso não
+            //ir para a tela de fim de jogo
+        } else {
+            //perder
+            id += 1;
+            if (levels[id]) {
+                setLevel();
+                btnContainer.style.pointerEvents = "auto";
+                btnContainer.style.opacity = "1";
+            } else {
+                endGame();
+            }
+            //verificar se ainda há perguntas
+            //caso sim
+            //ir para a próxima pergunta
+            //destravar botões
+            //caso não
+            //ir para a tela de fim de jogo
+        }
+    });
+}
+
+function endGame() {
+    translucentScreen();
+
+    let restartBtn = document.querySelector("#restart");
+    let menuBtn = document.querySelector("#back-menu");
+    let pointsLabel = document.querySelector("#points");
+    pointsLabel.innerText = "PONTOS: " + points;
+
+    setTimeout(startButtons, 1000);
+    function startButtons() {
+        pointsLabel.style.display = "block";
+        restartBtn.style.display = "block";
+        menuBtn.style.display = "block";
+    }
+
+    menuBtn.addEventListener("click", () => {
+        goTo("../");
+    });
+
+    restartBtn.addEventListener("click", () => {
+        goTo("./play.html");
+    });
+}
+
+function goTo(link) {
+    setTimeout(menu, 200);
+    function menu() {
+        let gameScreen = document.querySelector("#game-over");
+        gameScreen.style.display = "none";
+        sceneTransition(link, false);
+    }
+}
+
+function translucentScreen() {
+    let gameScreen = document.querySelector("#game-over");
+    gameScreen.style.display = "block";
+    setTimeout(() => {
+        gameScreen.style.background = "rgba(0, 0, 0, 0.75)";
+    }, 515);
+}
+// const imagem = level.drawning;
+// const arrayImgs = [];
+// const arrayFiles = level.imgs;
+// for (let i = 0; i <= arrayFiles.length - 1; i++) {
+//     let newImg = new Image();
+//     newImg.src = arrayFiles[i];
+//     arrayImgs.push(newImg);
+// }
+// let question = level.question;
+// let options = level.options;
+
+// options.sort((a, b) => 0.5 - Math.random());
+// let rightAnswers = options.filter((item) => item.isRight);
+// rightAnswers = rightAnswers.map((item) => item.text);
+
+// const questionText = document.querySelector("#question");
+// questionText.textContent = question;
+// const answers = document.querySelectorAll(".answer");
+// answers.forEach(defineAnswers);
+
+// function defineAnswers(item, index) {
+//     item.textContent = options[index].text;
+//     // item.addEventListener("click", () => winTest());
+//     item.addEventListener("click", winTest);
+
+//     function winTest() {
+//         if (rightAnswers.includes(item.textContent)) {
+//             setTimeout(win, 250);
+//             function win() {
+//                 victories = Number(victories) + 1
+//                 let gameResult = document.querySelector("#game-result");
+//                 gameResult.style.animationName = "openIcon";
+//                 gameResult.style.display = "block";
+//                 let positive = document.querySelector("#positive");
+//                 positive.style.display = "block";
+//                 setTimeout(endGameScreen, 500);
+//             }
+//         } else {
+//             setTimeout(defeat, 250);
+//             function defeat() {
+//                 let gameResult = document.querySelector("#game-result");
+//                 gameResult.style.animationName = "closeIcon";
+//                 gameResult.style.display = "block";
+//                 let negative = document.querySelector("#negative");
+//                 negative.style.display = "block";
+//                 setTimeout(startAnimation, 500);
+//             }
+//         }
+//     }
+// }
+
+// let canvas = document.querySelector("#animation");
+// let context = canvas.getContext("2d");
+// let imgHeight = (canvas.height / 100) * 75;
+// let imgWidth = (canvas.width / 100) * 50;
+// let img = new Image();
+// img.src = imagem;
+// img.onload = function () {
+//     context.drawImage(
+//         img,
+//         canvas.width / 2 - imgWidth / 2,
+//         canvas.height - imgHeight,
+//         imgWidth,
+//         imgHeight + 5
+//     );
+// };
+
+// //Colocar dentro da função
+// function startAnimation() {
+//     let indice = 0;
+//     let timeLapse = 0;
+//     let anim = true;
+//     function animation() {
+//         if (indice < arrayImgs.length - 1) {
+//             context.clearRect(0, 0, canvas.width, canvas.height);
+//             context.drawImage(
+//                 arrayImgs[Math.ceil(indice)],
+//                 canvas.width / 2 - imgWidth / 2,
+//                 canvas.height - imgHeight,
+//                 imgWidth,
+//                 imgHeight + 5
+//             );
+//             indice += level.animVel;
+//         } else {
+//             indice = 0;
+//             timeLapse += 1;
+//             if (timeLapse == 1) {
+//                 if (level.continueAnim === false) {
+//                     anim = false;
+//                 }
+//                 endGameScreen();
+//             }
+//         }
+//         if (anim === true) {
+//             requestAnimationFrame(animation);
+//         }
+//     }
+//     function start() {
+//         requestAnimationFrame(animation);
+//     }
+//     return start();
+// }
+// function endGameScreen() {
+//     if (levels.length > id) {
+//         sceneTransition("./scene.html?" + (Number(id) + 1) + "?" + victories);
+//     } else {
+//         translucentScreen();
+
+//         let restartBtn = document.querySelector("#restart");
+//         let menuBtn = document.querySelector("#back-menu");
+//         let pointsLabel = document.querySelector("#points");
+//         pointsLabel.innerText = "PONTOS: " + victories
+
+//         setTimeout(startButtons, 1000);
+//         function startButtons() {
+//             pointsLabel.style.display = "block";
+//             restartBtn.style.display = "block";
+//             menuBtn.style.display = "block";
+//         }
+
+//         restartBtn.addEventListener("click", () => {
+//             restartScene();
+//         });
+//         menuBtn.addEventListener("click", () => {
+//             mainMenu();
+//         });
+
+//         function restartScene() {
+//             setTimeout(restart, 200);
+//             function restart() {
+//                 let gameScreen = document.querySelector("#game-over");
+//                 gameScreen.style.display = "none";
+//                 sceneTransition(window.location.href, false);
+//             }
+//         }
+
+//         function mainMenu() {
+//             setTimeout(menu, 200);
+//             function menu() {
+//                 let gameScreen = document.querySelector("#game-over");
+//                 gameScreen.style.display = "none";
+//                 sceneTransition("../index.html", false);
+//             }
+//         }
+//     }
+// }
--- levels.js
+++ levels.js
@@ -1,138 +1,93 @@
 const levels = [
     {
-        id: 1,
-        icon: "animations/scales/stand0.png",
-        drawning: "../animations/scales/stand0.png",
-        imgs: imgGet("stand0", 0),
-        continueAnim: false,
-        animVel: 0.2,
         question: "O que é racismo?",
         options: [
             {
                 text: "Preferência por um determinado grupo social",
-                isRight: false,
             },
             {
                 text: "Crença na superioridade de um grupo racial sobre outro, acompanhada de práticas discriminatórias",
-                isRight: true,
             },
             {
                 text: "Interesse por diferentes culturas",
-                isRight: false,
             },
             {
                 text: "Respeito às diferenças individuais",
-                isRight: false,
             },
         ],
+        rightOption: 2,
     },
     {
-        id: 2,
-        icon: "animations/scales/stand0.png",
-        drawning: "../animations/scales/stand0.png",
-        imgs: imgGet("stand0", 0),
-        continueAnim: false,
-        animVel: 0.2,
         question: "Qual a principal causa do racismo?",
         options: [
             {
                 text: "Diferenças físicas entre as pessoas",
-                isRight: false,
             },
             {
                 text: "Construções sociais e históricas que atribuem valores diferentes a grupos raciais",
-                isRight: true,
             },
             {
                 text: "Convivência entre diferentes culturas",
-                isRight: false,
             },
             {
                 text: "Competição por recursos",
-                isRight: false,
             },
         ],
+        rightOption: 2,
     },
     {
-        id: 3,
-        icon: "animations/scales/stand0.png",
-        drawning: "../animations/scales/stand0.png",
-        imgs: imgGet("stand0", 0),
-        continueAnim: false,
-        animVel: 0.2,
         question: "Quais são algumas das consequências do racismo?",
         options: [
             {
                 text: "Desigualdade social, discriminação e violência",
-                isRight: true,
             },
             {
                 text: "Desenvolvimento de novas culturas e nações",
-                isRight: false,
             },
             {
                 text: "Fortalecimento da democracia e cidadania",
-                isRight: false,
             },
             {
                 text: "Aumento da tolerância e igualdade",
-                isRight: false,
             },
         ],
+        rightOption: 1,
     },
     {
-        id: 4,
-        icon: "animations/scales/stand0.png",
-        drawning: "../animations/scales/stand0.png",
-        imgs: imgGet("stand0", 0),
-        continueAnim: false,
-        animVel: 0.2,
         question: "O que podemos fazer para combater o racismo?",
         options: [
             {
                 text: "Ignorar as situações de racismo",
-                isRight: false,
             },
             {
                 text: "Promover o diálogo, a educação e a igualdade",
-                isRight: true,
             },
             {
                 text: "Reforçar os estereótipos",
-                isRight: false,
             },
             {
                 text: "Isolar as pessoas que praticam o racismo",
-                isRight: false,
             },
         ],
+        rightOption: 2,
     },
     {
-        id: 5,
-        icon: "animations/scales/stand0.png",
-        drawning: "../animations/scales/stand0.png",
-        imgs: imgGet("stand0", 0),
-        continueAnim: false,
-        animVel: 0.2,
         question: "Por que é importante falar sobre racismo?",
         options: [
             {
                 text: "Para culpar determinadas pessoas",
-                isRight: false,
             },
             {
                 text: "Para criar divisão entre as pessoas",
-                isRight: false,
             },
             {
                 text: "Para promover a conscientização e a mudança social",
-                isRight: true,
             },
             {
                 text: "Para generalizar comportamentos",
-                isRight: false,
             },
         ],
+        rightOption: 3,
     },
 ];
 
